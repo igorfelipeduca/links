48a461bfc4920788f55c4c3985c251d64ab7b5d9
Igor F. Duca <yelldutz@gmail.com>
2024-06-15 19:37:33 -0300

    feat: cv link

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Link from "./components/Link";
 import { Links } from "./constants/links";
 
 function App() {
@@ -13,29 +14,7 @@ function App() {
       <div className="mt-8 h-fit w-full flex items-center justify-center">
         <div className="flex flex-col space-y-4">
           {Links.map((link, index) => (
-            <a
-              key={index}
-              className="text-zinc-300 text-left text-md py-2 px-4 rounded-lg border border-zinc-700 flex justify-between w-60 group hover:text-white hover:underline"
-              href={link.url}
-              target="_blank"
-            >
-              {link.name}
-
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                className="size-5"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
-                />
-              </svg>
-            </a>
+            <Link key={index} title={link.name} href={link.url} />
           ))}
         </div>
       </div>
--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,26 +1,27 @@
 export const Links = [
-    {
-        name: "LinkedIn",
-        url: "https://www.linkedin.com/in/igorfelipeduca",
-    },
-    {
-        name: "GitHub",
-        url: "https://github.com/igorfelipeduca",
-    },
-    {
-        name: "Twitter",
-        url: "https://twitter.com/ducaswtf",
-    },
-    {
-        name: "Read my articles",
-        url: "https://articles.duca.dev",
-    },
-    {
-        name: "Dev.to",
-        url: "https://dev.to/yelldutz",
-    },
-    {
-        name: "My CV",
-        url: "https://read.cv/duca",
-    }
-]
+  {
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/igorfelipeduca",
+  },
+  {
+    name: "GitHub",
+    url: "https://github.com/igorfelipeduca",
+  },
+  {
+    name: "Twitter",
+    url: "https://twitter.com/ducaswtf",
+  },
+  {
+    name: "Read my articles",
+    url: "https://articles.duca.dev",
+  },
+  {
+    name: "Dev.to",
+    url: "https://dev.to/yelldutz",
+  },
+  {
+    name: "Download my CV",
+    url: "https://drive.google.com/uc?export=download&id=1-tFLxIFpqOrVUecrwRKIOlS2z7pWcsqB",
+    type: "download",
+  },
+];
